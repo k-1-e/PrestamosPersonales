60c19977f5da5ea98eeff43dfa8d7cd5d6ecf6aa
k-1-e <obregonkevin1351@gmail.com>
2021-09-03 21:43:03 -0600

    seccion de login lista

--- app/src/main/java/net/larntech/prestamospersonales/MainActivity.kt
+++ app/src/main/java/net/larntech/prestamospersonales/MainActivity.kt
@@ -1,11 +1,33 @@
 package net.larntech.prestamospersonales
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun startAplication(view: View) {
+        var usuario = findViewById<EditText>(R.id.EditUsuario).text.toString();
+        var contraseña = findViewById<EditText>(R.id.EditContraseña).text.toString();
+        if (usuario == "Kevin" && contraseña == "1351") {
+            val intent = Intent(this, Principal::class.java)
+            startActivity(intent)
+        }
+        else {
+
+            Toast.makeText(this,"Falta llenar los campos", Toast.LENGTH_SHORT)
+                .show();
+
+        }
+
+    }
+
+
 }
---
+++ app/src/main/java/net/larntech/prestamospersonales/Principal.kt
@@ -0,0 +1,14 @@
+package net.larntech.prestamospersonales
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class Principal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+    }
+   
+}
